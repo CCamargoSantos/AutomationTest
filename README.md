15e224d0417949869c063648b22d7dcf3b1fbc01
Camilo Camargo <ing.camargo.santos@gmail.com>
2022-02-24 00:04:16 -0500

    Uploads the automation project

---
+++ README.md
@@ -0,0 +1,46 @@
+# Search Validation
+It contains a python script that runs the requested assignment for the QA automation process
+
+It tried to do as follows:
+1. Go to https://www.microsoft.com/es-mx/
+2. Go to Windows
+3. Go to Search
+4. Search for “Xbox” and click on "Comprar"
+5. Once in the result page you will see "Aplicaciones" click on it
+6. Count the elements on the first 3 pages and print the sum of elements and all the titles
+7. Go back to the first page and select the first NON-FREE option and STORE the price for later comparison
+8. If you see a "Registration" pop up, close it
+9. In this page, you will see the price again, compare first price vs current prince and they should match
+10. Click on the "3 dot" button next to "Comprar" button and add the item to the CART
+11. Verify the app takes you to the Shopping Cart page and verify you have one element available
+12. Delete the item and verify you have 0 elements: "Tu carro está vacío" message should be present
+
+
+## Getting Started
+---
+Make sure you have Python 3.8.0 or newer installed and running on your machine. Open a terminal and 
+browse to the project's root folder. Start the program by running the following command.
+```
+python3 dice 
+```
+You can also run the program from an IDE like Visual Studio Code. Start your IDE and open the 
+project folder. Select the main module inside the dice folder and click the "run" button.
+
+## Project Structure
+---
+The project files and folders are organized as follows:
+```
+root                    (project root folder)
++-- Search_Validation.py (Automation script structure)
++-- chromedriver.exe      (Critical driver for chrome)             
++-- README.md           (general info)
+```
+
+## Required Technologies
+---
+* Python 3.8.0
+* chromedriver.exe (must be in the same folder of the runing script)
+
+## Authors
+---
+* Camilo Camargo
---
+++ Search_Validation.py
@@ -0,0 +1,130 @@
+from xmlrpc.client import Boolean
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.common.by import By
+import selenium.webdriver.support.ui as ui
+import selenium.webdriver.support.expected_conditions as ec
+import os
+import time
+
+
+
+class xboxaplicationsTest():
+    
+    def __init__(self):
+        options = webdriver.ChromeOptions()
+        options.add_argument('--ignore-certificate-errors')
+        options.add_argument('--ignore-ssl-errors')
+        options.add_experimental_option('excludeSwitches', ['enable-logging'])
+        self.driver = webdriver.Chrome(options=options)
+    
+    
+    def openbrowserandurl(self):
+        self.driver.maximize_window()
+        self.driver.get('https://www.microsoft.com/es-mx/')
+
+        #Click on 'Windows' link
+        ui.WebDriverWait(self.driver,15).until(ec.visibility_of_element_located((By.CSS_SELECTOR,'#shellmenu_2')))
+        self.driver.find_element(by=By.CSS_SELECTOR,value='#shellmenu_2').click()
+        ui.WebDriverWait(self.driver,15).until(ec.visibility_of_element_located((By.CSS_SELECTOR,'#search')))
+        
+        #Click on 'search' image
+        self.driver.find_element(by=By.CSS_SELECTOR,value='#search').click()
+        time.sleep(2)
+        
+        #Set the text 'Xbox' in searching bar
+        self.driver.find_element(by=By.CSS_SELECTOR,value= '#cli_shellHeaderSearchInput').send_keys('Xbox')
+        self.driver.find_element(by=By.CSS_SELECTOR,value='#search').click()
+        
+        #Handle a change of country market pop up and keep the MX market
+        try:
+            self.driver.find_element(by=By.CSS_SELECTOR,value='#R1MarketRedirect-close').click()
+            time.sleep(2)
+        except:
+            print('It will keep in MX webpage')
+        
+        #Click on 'buying' link
+        self.driver.find_element(by=By.CSS_SELECTOR,value='#coreui-pivotheader-s3sdtmy > div > div > header > a:nth-child(1)').click()
+        ui.WebDriverWait(self.driver,15).until(ec.visibility_of_element_located((By.CSS_SELECTOR,'#refine-by-menu-title-Categorías > div > ul > li:nth-child(1) > a')))
+        
+        #Click on 'Applications' link 
+        self.driver.find_element(by=By.CSS_SELECTOR,value='#refine-by-menu-title-Categorías > div > ul > li:nth-child(1) > a').click()
+        ui.WebDriverWait(self.driver,15).until(ec.visibility_of_element_located((By.CSS_SELECTOR,'#coreui-productplacementlist-1g76zxk > div:nth-child(3) > div')))
+        time.sleep(2)
+        
+        #store items in page 1
+        elements = self.driver.find_element(by=By.CSS_SELECTOR,value='#coreui-productplacementlist-1g76zxk > div:nth-child(3) > div')
+        i = 0
+        titles = []
+        for allitmes in elements.find_elements(by = By.XPATH,value = '//*') :
+            i+=1
+            titles.append(allitmes)
+        a = i/90
+        print(f'There are {i/a} elements in the first page')
+        #print(titles)
+        time.sleep(3)
+        
+        #store items in page 2
+        self.driver.find_element(by=By.CSS_SELECTOR,value= '#coreui-productplacementlist-1g76zxk > div:nth-child(3) > ul > li:nth-child(3) > a').click()
+        elements = self.driver.find_element(by=By.CSS_SELECTOR,value='#coreui-productplacementlist-1g76zxk > div:nth-child(3) > div')
+        i = 0
+        titles = []
+        for allitmes in elements.find_elements(by = By.XPATH,value = '//*') :
+            i+=1
+            titles.append(allitmes)
+        a = i/90
+        print(f'There are {i/a} elements in the second page')
+        #print(titles)
+        time.sleep(3)
+        
+        #store items in page 3
+        self.driver.find_element(by=By.CSS_SELECTOR,value= '#coreui-productplacementlist-1g76zxk > div:nth-child(3) > ul > li:nth-child(4) > a').click()
+        elements = self.driver.find_element(by=By.CSS_SELECTOR,value='#coreui-productplacementlist-1g76zxk > div:nth-child(3) > div')
+        i = 0
+        titles = []
+        for allitmes in elements.find_elements(by = By.XPATH,value = '//*') :
+            i+=1
+            titles.append(allitmes)
+        a = i/90
+        print(f'There are {i/a} elements in the third page')
+        #print(titles)
+        time.sleep(3)
+
+        #Back into page 1 and finds the first NON-FREE item, and selects it
+        self.driver.find_element(by=By.CSS_SELECTOR,value= '#coreui-productplacementlist-1g76zxk > div:nth-child(3) > ul > li:nth-child(2) > a').click()
+        elements = self.driver.find_element(by=By.CSS_SELECTOR,value='#coreui-productplacementlist-1g76zxk > div:nth-child(3) > div')
+        i = 0
+        titles = []
+        for allitmes in elements.find_elements(by = By.PARTIAL_LINK_TEXT,value = 'MXN') :
+            titles.append(allitmes)
+        element = titles[0]
+        element.click()
+        time.sleep(10)
+        
+        #Handle Registering pop up and close it
+        try:
+            self.driver.find_element(by=By.CSS_SELECTOR,value='#email-newsletter-dialog > div.sfw-dialog > div.c-glyph.glyph-cancel').click()
+            time.sleep(2)
+        except:
+            print('Registering pop up got closed')
+        
+        #Click on 3 dot button and add the item to the shopping cart 
+        elements = self.driver.find_element(by=By.CSS_SELECTOR,value= '#ButtonPanel_buttonPanel_OverflowMenuTrigger')
+        self.driver.find_element(by=By.CSS_SELECTOR,value= '#ButtonPanel_buttonPanel_OverflowMenuTrigger').click()
+        elements.find_elements(by = By.LINK_TEXT, value = 'Agregar al carro').click()
+
+        #Open the shopping cart and eliminate the item
+        ui.WebDriverWait(self.driver,15).until(ec.visibility_of_element_located((By.CSS_SELECTOR,'#store-cart-root > div > div > div > section.main--klSNnr0K > div > div > div > div > div > div.itemDetailsAndLegal--ULuWkT5t > div.item-details > div.item-quantity-price > div.item-quantity > div > div:nth-child(2)')))
+        element = self.driver.find_element(by = By.CSS_SELECTOR,value ='#store-cart-root > div > div > div > section.main--klSNnr0K > div > div > div > div > div > div.itemDetailsAndLegal--ULuWkT5t > div.item-details > div.item-quantity-price > div.item-quantity > div > div:nth-child(2)')
+        print(element.text)
+        time.sleep(5)
+        self.driver.find_element(by= By.CSS_SELECTOR,value= '#store-cart-root > div > div > div > section.main--klSNnr0K > div > div > div > div > div > div.itemDetailsAndLegal--ULuWkT5t > div.legalAndStatementContainer--dz8EQOG7 > div > button:nth-child(1)').click()
+        ui.WebDriverWait(self.driver,15).until(ec.visibility_of_element_located((By.CSS_SELECTOR,'#store-cart-root > div > div > div > section.main--klSNnr0K > p')))
+        element = self.driver.find_element(y= By.CSS_SELECTOR,value= '#store-cart-root > div > div > div > section.main--klSNnr0K > p')
+        print(element.text)
+
+
+if __name__ == "__main__":
+    obj = xboxaplicationsTest()
+    obj.openbrowserandurl()
